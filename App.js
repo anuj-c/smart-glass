b9f52ecf0f3fa6d1c9d49c9ade5c503910fb8e16
Anuj Choure <cam12@iitbbs.ac.in>
2024-01-25 16:02:37 +0530

    Working this

--- App.js
+++ App.js
@@ -1,27 +1,59 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {UvcCamera} from 'react-native-uvc-camera';
 
 const App = () => {
+  const [view, setView] = useState(false);
+
   return (
-    <View>
-      <Text>Hello World</Text>
-      <View>
-        <UvcCamera
-          ref={this.refCamera}
-          style={styles.camera}
-          rotation={270}
-          // type={UvcCamera.Constants.Type.back}
-        />
+    <View style={[styles.container]}>
+      <View style={[styles.text]}>
+        <Text style={[styles.textStyles]}>Hello World</Text>
+      </View>
+      <View style={[{marginBottom: 50}, styles.viewContainer]}>
+        {view && (
+          <UvcCamera ref={this.refCamera} style={styles.camera} rotation={90} />
+        )}
+      </View>
+      <View style={[styles.button]}>
+        <Button title="Take Picture" onPress={() => setView(true)} />
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
   camera: {
+    position: 'relative',
+    width: 240,
+    height: 370,
+    transform: [{rotate: '90deg'}],
+  },
+  viewContainer: {
+    position: 'absolute',
+    top: 0,
+    marginLeft: 80,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 0,
     width: '100%',
-    height: '100%',
+    padding: 10,
+  },
+  text: {
+    backgroundColor: 'skyblue',
+    marginBottom: 20,
+  },
+  textStyles: {
+    color: 'black',
+    fontWeight: 600,
+    fontSize: 30,
+    textAlign: 'center',
   },
 });
 
--- android/app/src/main/java/com/smart_g/MainActivity.kt
+++ android/app/src/main/java/com/smart_g/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.smart_g
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,11 +23,76 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "smart_g"
+  private val actionUsbPermission = "com.smart_g.USB_PERMISSION"
+  private lateinit var usbManager: UsbManager
+  private lateinit var usbReceiver: BroadcastReceiver
+
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      Log.d("TAG", "onCreate: MainActivity created")
+
+      usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+      val permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(actionUsbPermission), PendingIntent.FLAG_IMMUTABLE)
+      val filter = IntentFilter(actionUsbPermission)
+
+      usbReceiver = object : BroadcastReceiver() {
+          @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+          override fun onReceive(context: Context, intent: Intent) {
+              Log.d("TAG", "BroadcastReceiver onReceive")
+              when (intent.action) {
+                  actionUsbPermission -> {
+                      Log.d("TAG", "BroadcastReceiver onReceive2")
+                      synchronized(this) {
+                          val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+                          if (device != null) {
+                              Log.d("TAG", "Device: ${device.deviceName}")
+                          }
+                          if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                              device?.apply {
+                                  Log.d("TAG", "USB Permission Granted for device: ${device.deviceName}")
+                                  // Permission granted, handle the device
+                              }
+                          } else {
+                              Log.d("TAG", "USB Permission Denied for device: ${device?.deviceName}")
+                          }
+                      }
+                  }
+                  UsbManager.ACTION_USB_DEVICE_ATTACHED -> {
+                      val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+                      if (device != null) {
+                          Log.d("TAG", "USB attached: ${device.deviceName}")
+                      }
+                      device?.apply {
+                          usbManager.requestPermission(device, permissionIntent)
+                      }
+                  }
+              }
+          }
+      }
+
+      registerReceiver(usbReceiver, filter)
+
+      Log.d("TAG", "BroadcastReceiver registered for USB Permission")
+  }
+
+  override fun onResume() {
+      super.onResume()
+      val filter = IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+      registerReceiver(usbReceiver, filter)
+      Log.d("TAG", "BroadcastReceiver registered for USB Device Attached")
+  }
+
+  override fun onPause() {
+      super.onPause()
+      unregisterReceiver(usbReceiver)
+      Log.d("TAG", "BroadcastReceiver unregistered")
+  }
 }
